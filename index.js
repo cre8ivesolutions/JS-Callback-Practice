64a6bca7ecced6625513083e6a75963e608dc927
Tammy Swierczynski <tammerz78@gmail.com>
2022-06-06 11:06:33 -0500

    character not defined error

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const inventory = newInventory()
 move(inventory).to(0, 0)
 
 const character = newImage('assets/green-character/static.gif')
-move(character).to(100, 250)
 
+move(character).to(100, 250)  
 
 move(newImage('assets/tree.png')).to(200, 450)
 move(newImage('assets/pillar.png')).to(350, 250)
@@ -13,3 +13,12 @@ move(newImage('assets/well.png')).to(500, 575)
 move(newItem('assets/sword.png')).to(500, 555)
 move(newItem('assets/shield.png')).to(165, 335)
 move(newItem('assets/staff.png')).to(600, 250)
+
+
+// The code in each file will run in the following order:
+
+// newImage.js
+// newItem.js
+// newInventory.js
+// move.js
+// index.js
--- move.js
+++ move.js
@@ -1,12 +1,42 @@
-function move(element) {
-    element.style.position = 'fixed'
 
+function move(element) {
+    
+    element.style.position = 'fixed'
+    
     function moveToCoordinates(left, bottom) {
         element.style.left = left + 'px'
         element.style.bottom = bottom + 'px'
     }
+    
+    function moveCharacter(){ 
+        let direction = null;
+        let x = 100;
+        let y = 250;
+        
+        if(direction === 'west'){
+            x = x - 1
+        }
+        if(direction === 'north'){
+            y = y + 1
+        }
+        if(direction === 'east'){
+            x = x + 1
+        }
+        if(direction === 'south'){
+            y = y - 1
+        }
+        element.style.left = x + 'px'
+        element.style.bottom = y + 'px'
+    }
+    setInterval(moveCharacter), 1
 
     return {
-        to: moveToCoordinates
+        to: (moveToCoordinates)
     }
 }
+
+// move(character,east)
+
+
+
+// move(inventory).to(0, 0)
